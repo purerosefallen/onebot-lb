8629567d9bc817b60890c0f29c76dd4a6f3d6e49
purerosefallen <78877@qq.com>
2021-10-29 13:04:08 +0800

    remove

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,9 @@ import { OnebotGateway } from './onebot.gateway';
 import { MessageService } from './message/message.service';
 import { ReverseWsService } from './reverse-ws/reverse-ws.service';
 import { WaitBotService } from './wait-bot/wait-bot.service';
-import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
-    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       ignoreEnvVars: true,
       load: [loadConfig],
